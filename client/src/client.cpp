75f3c471a5cfed23bed680883596b8a391076d2f
iceman2146 <iceman2146@mail.ru>
2023-10-22 16:59:04 +0300

    fx

--- client/src/client.cpp
+++ client/src/client.cpp
@@ -1,13 +1,13 @@
-#include<iostream>
-#include<queue>
-#include<string>
-#include<cstdlib>
+#include <cstdlib>
+#include <iostream>
+#include <queue>
+#include <string>
 
-#include<boost/thread.hpp>
-#include<boost/bind/bind.hpp>
-#include<boost/asio.hpp>
-#include<boost/asio/ip/tcp.hpp>
-#include<boost/algorithm/string.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/asio.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/bind/bind.hpp>
+#include <boost/thread.hpp>
 
 using namespace std;
 using namespace boost;
@@ -16,145 +16,144 @@ using namespace boost::asio::ip;
 
 typedef boost::shared_ptr<tcp::socket> socket_ptr;
 typedef boost::shared_ptr<string> string_ptr;
-typedef boost::shared_ptr< queue<string_ptr> > messageQueue_ptr;
+typedef boost::shared_ptr<queue<string_ptr>> messageQueue_ptr;
 
 io_service service; // Boost Asio io_service
-messageQueue_ptr messageQueue(new queue<string_ptr>); // Queue for producer consumer pattern
-tcp::endpoint ep(ip::address::from_string("127.0.0.1"), 8001); // TCP socket for connecting to server
+messageQueue_ptr
+    messageQueue(new queue<string_ptr>); // Queue for producer consumer pattern
+tcp::endpoint ep(ip::address::from_string("127.0.0.1"),
+                 8001);    // TCP socket for connecting to server
 const int inputSize = 256; // Maximum size for input buffer
-string_ptr promptCpy; // Terminal prompt displayed to chat users
+string_ptr promptCpy;      // Terminal prompt displayed to chat users
 
 // Function Prototypes
 bool isOwnMessage(string_ptr);
 
-void displayLoop(socket_ptr);//Удаляет элементы данных из messageQueue для отображения в клиентском терминале; т.е. потребитель
+void displayLoop(
+    socket_ptr); // Удаляет элементы данных из messageQueue для отображения в
+                 // клиентском терминале; т.е. потребитель
 
-void inboundLoop(socket_ptr, string_ptr);	//Будет отправлять элементы из сокета в нашу messageQueue; т.е. производитель
-											//создает цикл, который вставляется в поток только тогда, когда сообщение 
-											//доступно в сокете, подключенном к серверу. Чтение из объекта сокета — это операция, 
-											//которая потенциально может помешать записи в сокет, поэтому мы устанавливаем задержку
-											// в одну секунду для проверок на чтение.
+void inboundLoop(
+    socket_ptr,
+    string_ptr); // Будет отправлять элементы из сокета в нашу messageQueue;
+                 // т.е. производитель создает цикл, который вставляется в поток
+                 // только тогда, когда сообщение доступно в сокете,
+                 // подключенном к серверу. Чтение из объекта сокета — это
+                 // операция, которая потенциально может помешать записи в
+                 // сокет, поэтому мы устанавливаем задержку
+                 //  в одну секунду для проверок на чтение.
 
-void writeLoop(socket_ptr, string_ptr);	//записи mesasges в сокет для отправки другим участникам сеанса чата, нам нужен цикл, который 
-										//будет постоянно опрашивать пользовательский ввод. После того, как пользовательский ввод будет
-										//прочитан, запишите сообщение в сокет и дождитесь следующего ввода. Вспомните, что эта операция 
-										//является многопоточной, поэтому входящие сообщения все еще могут отображаться, поскольку это 
-										//происходит в совершенно другом потоке.
+void writeLoop(
+    socket_ptr,
+    string_ptr); // записи mesasges в сокет для отправки другим участникам
+                 // сеанса чата, нам нужен цикл, который будет постоянно
+                 // опрашивать пользовательский ввод. После того, как
+                 // пользовательский ввод будет прочитан, запишите сообщение в
+                 // сокет и дождитесь следующего ввода. Вспомните, что эта
+                 // операция является многопоточной, поэтому входящие сообщения
+                 // все еще могут отображаться, поскольку это происходит в
+                 // совершенно другом потоке.
 
-string* buildPrompt();	// обрабатывает отображение входных данных терминала для клиентов.
-						//принимает строку имени клиента и присваивает
-						//ее значению указателя приглашения, который мы объявили ранее.
+string *
+buildPrompt(); // обрабатывает отображение входных данных терминала для
+               // клиентов.
+               // принимает строку имени клиента и присваивает
+               // ее значению указателя приглашения, который мы объявили ранее.
 
 // End of Function Prototypes
 
-int main(int argc, char** argv)
-{
-	try
-	{
-		boost::thread_group threads;
-		socket_ptr sock(new tcp::socket(service));
+int main(int argc, char **argv) {
+  try {
+    boost::thread_group threads;
+    socket_ptr sock(new tcp::socket(service));
 
-		string_ptr prompt( buildPrompt() );
-		promptCpy = prompt;
+    string_ptr prompt(buildPrompt());
+    promptCpy = prompt;
 
-		sock->connect(ep);
+    sock->connect(ep);
 
-		cout << "Welcome to the ChatServer\nType \"exit\" to quit" << endl;
+    cout << "Welcome to the ChatServer\nType \"exit\" to quit" << endl;
 
-		threads.create_thread(boost::bind(displayLoop, sock));
-		threads.create_thread(boost::bind(inboundLoop, sock, prompt));
-		threads.create_thread(boost::bind(writeLoop, sock, prompt));
+    threads.create_thread(boost::bind(displayLoop, sock));
+    threads.create_thread(boost::bind(inboundLoop, sock, prompt));
+    threads.create_thread(boost::bind(writeLoop, sock, prompt));
 
-		threads.join_all();
-	}
-	catch(std::exception& e)
-	{
-		cerr << e.what() << endl;
-	}
+    threads.join_all();
+  } catch (std::exception &e) {
+    cerr << e.what() << endl;
+  }
 
-	puts("Press any key to continue...");
-	getc(stdin);
-	return EXIT_SUCCESS;
+  puts("Press any key to continue...");
+  getc(stdin);
+  return EXIT_SUCCESS;
 }
 
-string* buildPrompt()
-{
-	const int inputSize = 256;
-	char nameBuf[inputSize] = {0};
-	string* prompt = new string(": ");
+string *buildPrompt() {
+  const int inputSize = 256;
+  char nameBuf[inputSize] = {0};
+  string *prompt = new string(": ");
 
-	cout << "Please input a new username: ";
-	cin.getline(nameBuf, inputSize);
-	*prompt = (string)nameBuf + *prompt;
-	boost::algorithm::to_lower(*prompt);
+  cout << "Please input a new username: ";
+  cin.getline(nameBuf, inputSize);
+  *prompt = (string)nameBuf + *prompt;
+  boost::algorithm::to_lower(*prompt);
 
-	return prompt;
+  return prompt;
 }
 
-void inboundLoop(socket_ptr sock, string_ptr prompt)
-{
-	int bytesRead = 0;
-	char readBuf[1024] = {0};
+void inboundLoop(socket_ptr sock, string_ptr prompt) {
+  int bytesRead = 0;
+  char readBuf[1024] = {0};
 
-	for(;;)
-	{
-		if(sock->available())
-		{
-			bytesRead = sock->read_some(buffer(readBuf, inputSize));
-			string_ptr msg(new string(readBuf, bytesRead));
+  for (;;) {
+    if (sock->available()) {
+      bytesRead = sock->read_some(buffer(readBuf, inputSize));
+      string_ptr msg(new string(readBuf, bytesRead));
 
-			messageQueue->push(msg);
-		}
+      messageQueue->push(msg);
+    }
 
-		boost::this_thread::sleep( boost::posix_time::millisec(int(1000)));
-	}
+    boost::this_thread::sleep(boost::posix_time::millisec(int(1000)));
+  }
 }
 
-void writeLoop(socket_ptr sock, string_ptr prompt)
-{
-	char inputBuf[inputSize] = {0};
-	string inputMsg;
+void writeLoop(socket_ptr sock, string_ptr prompt) {
+  char inputBuf[inputSize] = {0};
+  string inputMsg;
 
-	for(;;)
-	{
-		cin.getline(inputBuf, inputSize);
-		inputMsg = *prompt + (string)inputBuf + '\n';
+  for (;;) {
+    cin.getline(inputBuf, inputSize);
+    inputMsg = *prompt + (string)inputBuf + '\n';
 
-		if(!inputMsg.empty())
-		{
-			sock->write_some(buffer(inputMsg, inputSize));
-		}
+    if (!inputMsg.empty()) {
+      sock->write_some(buffer(inputMsg, inputSize));
+    }
 
-		if(inputMsg.find("exit") != string::npos)
-			exit(1);
+    if (inputMsg.find("exit") != string::npos)
+      exit(1);
 
-		inputMsg.clear();
-		memset(inputBuf, 0, inputSize);
-	}
+    inputMsg.clear();
+    memset(inputBuf, 0, inputSize);
+  }
 }
 
-void displayLoop(socket_ptr sock)
-{
-	for(;;)
-	{
-		if(!messageQueue->empty())
-		{
-			if(!isOwnMessage(messageQueue->front()))
-			{
-				cout << "\n" + *(messageQueue->front());
-			}
+void displayLoop(socket_ptr sock) {
+  for (;;) {
+    if (!messageQueue->empty()) {
+      if (!isOwnMessage(messageQueue->front())) {
+        cout << "\n" + *(messageQueue->front());
+      }
 
-			messageQueue->pop();
-		}
+      messageQueue->pop();
+    }
 
-		boost::this_thread::sleep( boost::posix_time::millisec(int(1000)));
-	}
+    boost::this_thread::sleep(boost::posix_time::millisec(int(1000)));
+  }
 }
 
-bool isOwnMessage(string_ptr message)
-{
-	if(message->find(*promptCpy) != string::npos)
-		return true;
-	else
-		return false;
+bool isOwnMessage(string_ptr message) {
+  if (message->find(*promptCpy) != string::npos)
+    return true;
+  else
+    return false;
 }
